fb50970b0dc897523c855f46bdf361b95363752f
ericung <ungericw@tutanota.com>
2024-02-13 15:52:10 -0600

    water 1

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+# References
+
+Zenodo. (2024). Zenodo Record. Retrieved from https://doi.org/10.5281/zenodo.6644169
--- test.py
+++ test.py
@@ -8,7 +8,5 @@ import pandas as pd
 import numpy as np
 import pyscf
 
-df = pd.DataFrame(np.ones((100,4)))
-df.to_pickle("water-x6.pkl")
-print(df.keys())
-print(df)
+df = pd.read_pickle("./water-x6.pkl")
+df
