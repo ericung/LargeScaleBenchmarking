0387cf7071c3441678b6146f9b3a42d706262912
ericung <ungericw@tutanota.com>
2024-02-13 15:24:50 -0600

    test py

---
+++ test.py
@@ -0,0 +1,13 @@
+try:
+    import cPickle as pickle
+except:
+    import pickle
+
+# 1.3.4
+import pandas as pd
+import numpy as np
+import pyscf
+
+df = pd.DataFrame(np.ones((100,4)))
+df.to_pickle("water-x6.pkl")
+print(df)
